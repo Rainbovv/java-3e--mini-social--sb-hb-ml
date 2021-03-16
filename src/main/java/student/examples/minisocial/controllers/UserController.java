ac4243cef85c2cd9ed3605f32721ce787846704f
Rainbovv <turcanu.radu17@gmail.com>
2021-03-16 23:23:41 +0200

    added jpaRepoImpl<User>; done entity<User>; ToDo: add all the methods

--- src/main/java/student/examples/minisocial/controllers/UserController.java
+++ src/main/java/student/examples/minisocial/controllers/UserController.java
@@ -2,6 +2,7 @@ package student.examples.minisocial.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import student.examples.minisocial.domain.entities.User;
@@ -20,8 +21,8 @@ public class UserController {
 		 return userService.getUsersList();
 	}
 
-	@GetMapping("/users/")
-	public User show(@RequestParam int id) {
+	@GetMapping("/users/{id}")
+	public User show(@PathVariable int id) {
 		return userService.getUserById(id);
 	}
 }
